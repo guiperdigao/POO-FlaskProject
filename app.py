a0571cf71de555314f9591d34eb394f0589087f1
gmrperdigao <36706143+gmrperdigao@users.noreply.github.com>
2018-06-28 04:44:26 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from googlemaps import convert
 from googlemaps.convert import as_list
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, flash, redirect, url_for, logging, make_response
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
 
 form = cgi.FieldStorage()
 print("Content-type: text/html")
@@ -18,11 +23,125 @@ app.config['GOOGLEMAPS_KEY'] = "AIzaSyC-jyfRJC7gRskgtLADyrOCod3EzhVYE7s"
 app.secret_key = "AIzaSyC-jyfRJC7gRskgtLADyrOCod3EzhVYE7s"
 GoogleMaps(app, key="AIzaSyC-jyfRJC7gRskgtLADyrOCod3EzhVYE7s")
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'triptipz'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
+
 @app.route('/')
-def get_form():
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/paper-plane.png')
+def root():
+    return app.send_static_file('paper-plane.png')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('signin.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('plan'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/plan')
+@is_logged_in
+def plan():
     return render_template('index.html')
 
 @app.route('/result', methods = ['POST','GET'])
+@is_logged_in
 def need_input():
     if request.method == 'POST':
         form = request.form
@@ -171,7 +290,7 @@ def need_input():
         lng= lng_o,
         markers=marcadores
     )
-    return render_template('example.html',trdmap=trdmap, dist = dist/1000, nome = data['nome'])
+    return render_template('route.html',trdmap=trdmap, dist = dist/1000, nome = data['nome'])
 
 
 if __name__ == "__main__":
